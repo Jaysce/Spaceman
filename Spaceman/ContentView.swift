252db1f25b8d0c032d6743c66697238e8a990e2b
Sasindu Jayasinghe <sasindu.jayasinghe@gmail.com>
2020-11-23 15:11:32 +1300

    Initial Commit

---
+++ Spaceman/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Spaceman
+//
+//  Created by Sasindu Jayasinghe on 23/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
